c96b88c2aa9363e8e52cd295bcb9fc236f61de87
KrystianSzynkiewicz <krystianszynkiewicz@gmail.com>
2024-07-12 13:08:54 +0200

    add project with cart

---
+++ js/custom-JS.js
@@ -0,0 +1,104 @@
+// ---------------------------------------------
+
+// GLIDER
+const gap = 16;
+
+const carousel = document.getElementById("carousel"),
+  content = document.getElementById("content"),
+  next = document.getElementById("next"),
+  prev = document.getElementById("prev");
+
+next.addEventListener("click", (e) => {
+  carousel.scrollBy(width + gap, 0);
+  if (carousel.scrollWidth !== 0) {
+    prev.style.display = "flex";
+  }
+  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "none";
+  }
+});
+prev.addEventListener("click", (e) => {
+  carousel.scrollBy(-(width + gap), 0);
+  if (carousel.scrollLeft - width - gap <= 0) {
+    prev.style.display = "none";
+  }
+  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "flex";
+  }
+});
+
+let width = carousel.offsetWidth;
+window.addEventListener("resize", (e) => (width = carousel.offsetWidth));
+
+// ---------------------------------------------
+
+// ICON-SLIDER
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
+  "--marquee-elements-displayed"
+);
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+
+// ---------------------------------------------
+
+// NEW-SLIDER
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
+
+// ---------------------------------------------
+
+// phone-MENU-listing
+
+const smallMenuButton = document.getElementById("smallMenuButton");
+const floatingMenu = document.getElementById("group-icon-header");
+
+smallMenuButton.addEventListener("click", function () {
+  floatingMenu.classList.add("active");
+});
+
+// ---------------------------------------------
+
+//popup-listing
+const POPUP = document.querySelector("#popup");
+const POPUP_CLOSE = document.querySelector("#popup-close");
+const POPUP_MORE = document.querySelector(".more-button-grid");
+
+POPUP_MORE.addEventListener("click", function () {
+  POPUP.classList.remove("deactive");
+});
+POPUP_CLOSE.addEventListener("click", function () {
+  POPUP.classList.add("deactive");
+});
+
+// ---------------------------------------------
